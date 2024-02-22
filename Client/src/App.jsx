0874ec4f87c20c305144a63e9f1e5e6908bd7333
Tamal <ckbtamaldipnew@gmail.com>
2024-02-22 21:00:36 +0600

    add game logic

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Square from './Square/Square'
 
@@ -12,6 +12,53 @@ function App() {
 
   const [gameState, setGameState] = useState(renderFrom);
   const [currentPlayer, setCurrentPlayer] = useState('circle');
+  const [finishedState, setFinishedState] = useState(false);
+
+  const checkWinner = () => {
+    //? row wise winning logic
+    for (let row = 0; row < gameState.length; row++) {
+      if (gameState[row][0] === gameState[row][1] && gameState[row][1] === gameState[row][2]) {
+        return gameState[row][0];
+      }
+    }
+
+    //? column wise winning logic
+    for (let col = 0; col < gameState.length; col++) {
+      if (gameState[0][col] === gameState[1][col] && gameState[1][col] === gameState[2][col]) {
+        return gameState[0][col];
+      }
+    }
+
+    //?cross wise winning logic
+    if (gameState[0][0] === gameState[1][1] && gameState[1][1] === gameState[2][2]) {
+      return gameState[0][0];
+    }
+    if (gameState[0][2] === gameState[1][1] && gameState[1][1] === gameState[2][0]) {
+      return gameState[0][2];
+    }
+
+    //? Match draw 
+    const isDrawMatch = gameState.flat().every((e) => {
+      if (e === 'circle' || e === 'cross') {
+        return true;
+      }
+    });
+
+    if (isDrawMatch) {
+      return "draw";
+    }
+
+    return null;
+  }
+
+  useEffect(() => {
+    const winner = checkWinner();
+
+    if (winner) {
+      setFinishedState(winner);
+    }
+  }, [gameState]);
+
 
   return (
     <>
@@ -23,18 +70,25 @@ function App() {
           </div>
           <h1 className='game-heading water-background'>Tic Tac Toe</h1>
           <div className='square-wrapper'>
-            {gameState.map(arr => {
-              return arr.map(e => {
+            {gameState.map((arr, rowIndex) => {
+              return arr.map((elem, colIndex) => {
                 return <Square
+                  finishedState={finishedState}
                   currentPlayer={currentPlayer}
                   setCurrentPlayer={setCurrentPlayer}
                   setGameState={setGameState}
-                  key={e}
-                  id={e} />;
+                  key={rowIndex * 3 + colIndex}
+                  id={rowIndex * 3 + colIndex} />;
               })
             }
             )}
           </div>
+          {finishedState && finishedState === "draw" &&
+            (<h3 className='finished-state'>It's a Draw</h3>)
+          }
+          {finishedState && finishedState !== "draw" &&
+            (<h3 className='finished-state'>{finishedState} won the game</h3>)
+          }
         </div>
       </div>
     </>
--- Client/src/Square/Square.jsx
+++ Client/src/Square/Square.jsx
@@ -3,20 +3,20 @@ import './Square.css'
 
 const circleSvg = (
     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
         <g
             id="SVGRepo_tracerCarrier"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
         ></g>
         <g id="SVGRepo_iconCarrier">
             {" "}
             <path
                 d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
                 stroke="#ffffff"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
             ></path>{" "}
         </g>
     </svg>
@@ -24,39 +24,58 @@ const circleSvg = (
 
 const crossSvg = (
     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
         <g
             id="SVGRepo_tracerCarrier"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
         ></g>
         <g id="SVGRepo_iconCarrier">
             {" "}
             <path
                 d="M19 5L5 19M5.00001 5L19 19"
                 stroke="#fff"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
             ></path>{" "}
         </g>
     </svg>
 );
 
-const Square = ({ setGameState, id, currentPlayer, setCurrentPlayer }) => {
+const Square = ({ setGameState, id, currentPlayer, setCurrentPlayer, finishedState }) => {
 
     const [icon, setIcon] = useState(null);
 
     const clickOnSquare = () => {
+
+        if (finishedState) {
+            return;
+        }
+
         if (!icon) {
             currentPlayer === 'circle' ? setIcon(circleSvg) : setIcon(crossSvg);
         }
 
+        const myCurrentPlayer = currentPlayer;
+
         setCurrentPlayer(currentPlayer === 'circle' ? 'cross' : 'circle');
+
+        setGameState(prevState => {
+            let newState = [...prevState]; //? Here newState is the logic board which will store all the move
+            const rowIndex = Math.floor(id / 3);
+            const colIndex = id % 3;
+
+            newState[rowIndex][colIndex] = myCurrentPlayer; //* Logical move storing 
+            // console.log(newState);
+
+            return newState;
+        });
     }
 
     return (
-        <div onClick={clickOnSquare} className='sqaure'>
+        <div onClick={clickOnSquare}
+            className={`sqaure ${finishedState ? 'not-allowed' : ''}`}>
             {
                 icon
             }
